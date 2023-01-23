ed2e7b8bebd1159aebe2fcbeb84e3822eda2dcdd
lmoreau21 <61840342+lmoreau21@users.noreply.github.com>
2023-01-23 08:02:16 -0600

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,33 +1,24 @@
-colorList = ["red","orange","yellow","lime","cyan","blue","magenta","brown","white","black"]
-size = 30
-curColor = "black"
-
 function setup() {
-  createCanvas(400, 400);
-  background(255);
-  stroke("white")
-  sqColor();
+  createCanvas(800, 400);
 }
 
-function draw() { 
+function draw() {
+  shapeWidth = 300;
+  background(0);
+  noStroke()
   
-  stroke(curColor)
-  strokeWeight(10);
-  if(mouseIsPressed){
-    if(mouseX <= size && mouseY <= 30*colorList.length){
-      index = (int) (mouseY/30)
-      print(colorList[index]);
-      curColor = colorList[index]
-    } else{
-  	  line(mouseX,mouseY,pmouseX,pmouseY) 
-    }
-  } 
-}
-
-function sqColor(){
-  for(var i=0; i< colorList.length; i++){
-    fill(colorList[i])
-    square(0,size*i,size)
-  }
+  fill("yellow")
+  arc(width/4,height/2,shapeWidth,shapeWidth,-3*PI/4,-5*PI/4,PIE)
   
+  fill("red")
+  rect(width/2+shapeWidth/6, height/2, shapeWidth,shapeWidth/2)
+  circle(width*3/4, height/2, shapeWidth)
+  
+  fill("white")
+  circle(width*3/4-shapeWidth/4, height/2, shapeWidth/3.5)
+  circle(width*3/4+shapeWidth/4, height/2, shapeWidth/3.5)
+  
+  fill("blue")
+  circle(width*3/4-shapeWidth/4, height/2, shapeWidth/6)
+  circle(width*3/4+shapeWidth/4, height/2, shapeWidth/6)
 }
